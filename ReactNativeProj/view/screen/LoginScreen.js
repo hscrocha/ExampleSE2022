43ad0d2ddcd9edc24ca58adc3953d6dfff2d67b0
Henrique Rocha <henrique.rocha@gmail.com>
2022-10-04 22:48:26 -0400

    Lecture 09 - Finalizing the React Basic Slides Login Form with States and submit

--- ReactNativeProj/view/screen/LoginScreen.js
+++ ReactNativeProj/view/screen/LoginScreen.js
@@ -1,15 +1,35 @@
 import React from "react";
-import { Text, TextInput, View } from "react-native";
+import { Button, Text, TextInput, TouchableOpacity, View } from "react-native";
 import MainStyle from "../MainStyle.style";
+import FormStyle from "../Form.style";
 
 export default function LoginScreen(){
+    const [email, setEmail] = React.useState("");
+    const [password, setPassword] = React.useState("");
+
+    function handleSubmit(pEmail, pPwd){
+        const user = {login: pEmail, password: pPwd};
+        // I would call a method from a different Layer
+        
+        console.log(JSON.stringify(user));
+        alert( JSON.stringify(user) );
+    }
+
     return (<>
-        <View style={MainStyle.centerView}>
+        <View style={FormStyle.groupView}>
             <Text style={MainStyle.emphasisText}> Login </Text>
         </View>
-        <View style={MainStyle.centerView}>
-            <Text>Email:</Text>
-            <TextInput style={{width:'90%',borderWidth:1, borderRadius:10, padding:10}} />
+        <View style={FormStyle.groupView}>
+            <Text style={FormStyle.label}>Email:</Text>
+            <TextInput onChangeText={setEmail} style={FormStyle.input} autoCapitalize={false} />
+
+            <Text style={FormStyle.label}>Password:</Text>
+            <TextInput onChangeText={setPassword} style={FormStyle.input} secureTextEntry={true} />
+
+            <TouchableOpacity style={FormStyle.formButton} 
+                 onPress={()=> handleSubmit(email,password)}>
+                <Text style={FormStyle.formButtonText}> Submit </Text>
+            </TouchableOpacity>
         </View>
         </>
     );
